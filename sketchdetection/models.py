6fffd6fb66f1b77a0839fa75c16f4b2e68e5655c
Arian Bakh <arian@noklabs.ai>
2024-10-01 14:23:04 -0400

    Bug fix regarding pretrained weights

--- sketchdetection/models.py
+++ sketchdetection/models.py
@@ -10,7 +10,7 @@ def get_model(architecture: str):
             model = torchvision.models.resnet152(weights=torchvision.models.ResNet152_Weights.IMAGENET1K_V2)
             model.fc = torch.nn.Linear(2048, SKETCHY_CLASSES)
         case "EfficientNetV2L":
-            model = torchvision.models.efficientnet_v2_l()
+            model = torchvision.models.efficientnet_v2_l(weights=torchvision.models.EfficientNet_V2_L_Weights.IMAGENET1K_V1)
             model.classifier[1] = torch.nn.Linear(1280, SKETCHY_CLASSES)
         case _:
             raise ValueError(f"Unknown architecture {architecture}")
